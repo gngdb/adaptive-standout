72ed744ceea162859c20c789df5280ee21954375
Gavin <gavingray1729@gmail.com>
2015-10-12 16:38:00 +0100

    standout layer first version

--- standout/layers.py
+++ standout/layers.py
@@ -4,10 +4,38 @@ import lasagne.layers
 import theano
 import theano.tensor as T
 
-class Dropout(lasagne.layers.Dropout):
+from theano.sandbox.rng_mrg import MRG_RandomStreams
+_srng = MRG_RandomStreams(42)
+
+class Dropout(lasagne.layers.Layers):
     """
     Standout version of dropout. Operates in the same way as traditional 
     dropout, but this implementation allows a different dropout probability
     for each unit; and takes as an input the expression for each of these
-    dropout probability.
+    dropout probabilities.
+    Inputs:
+        * incoming - previous layer in the network.
+        * incoming_beliefnet - layer encoding dropout probabilities.
+        * rescale - after dropping out units, rescale other inputs
     """
+    def __init__(self, incoming, incoming_beliefnet, rescale=True, **kwargs):
+        lasagne.layers.Layer.__init__(self, incoming, **kwargs)
+        self.incoming_beliefnet = incoming_beliefnet
+
+    def get_output_for(self, input):
+        # get the probabilities from the beliefnet
+        self.p = self.incoming_beliefnet.get_output_for(input)
+        # sample a Bernoulli matrix using these probabilities
+        if deterministic or self.p == 0:
+            return input
+        else:
+            retain_prob = 1. - self.p
+            if self.rescale:
+                input \= retain_prob
+            # sample uniform and threshold to sample from many different 
+            # probabilities
+            self.uniform = _srng.uniform(self.input_shape)
+            # keep if less than retain_prob
+            self.mask = self.uniform > retain_prob
+            # then just apply mask to input
+            return input*self.mask
